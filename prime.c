9781e48503ca5828f35721ac89303bb53b0f72c0
fakhrulrazin <32606834+fakhrulrazin@users.noreply.github.com>
2018-10-05 00:56:52 +0800

    nice

--- prime.c
+++ prime.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 int main()
 {
-    int n, i, flag = 0;
+    int number, i, flag = 0;
 
     printf("Enter a positive integer: ");
-    scanf("%d", &n);
+    scanf("%d", &number);
 
-    for(i = 2; i <= n/2; ++i)
+    for(i = 2; i <= number/2; ++i)
     {
         // condition for nonprime number
-        if(n%i == 0)
+        if(number%i == 0)
         {
             flag = 1;
             break;
         }
     }
 
-    if (n == 1) 
+    if (number == 1) 
     {
       printf("1 is neither a prime nor a composite number.");
     }
     else 
     {
         if (flag == 0)
-          printf("%d is a prime number.", n);
+          printf("%d is a prime number.", number);
         else
-          printf("%d is not a prime number.", n);
+          printf("%d is not a prime number.", number);
     }
     
     return 0;
